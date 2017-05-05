da3528e6d4d4fcd7da0450ea2fb40b383d42daba
Bill Lugo <lugow1@mail.montclair.edu>
2017-05-05 01:37:37 -0400

    Initial commit

---
+++ app/src/main/java/laughingleviathon/minitoro_inclass/MainActivity.java
@@ -0,0 +1,14 @@
+package laughingleviathon.minitoro_inclass;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getActionBar().setTitle("web browser demo");
+    }
+}
